5dd95741edef658fb78d64858b3243b5b29d5273
kubotar <kubotar@nips-MacBook-Air.local>
2024-06-04 12:36:55 +0900

    test久保田

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <h1>testふじもり</h1>
+    <h1>test久保田</h1>
     <App />
   </React.StrictMode>,
 )
